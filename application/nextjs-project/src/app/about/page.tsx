e7cc5099f88ff06b11676549846bdf98a9874dac
SpindlyGold019 <58046529+SpindlyGold019@users.noreply.github.com>
2024-09-19 23:25:46 -0700

    Added metadata title.

--- application/nextjs-project/src/app/about/page.tsx
+++ application/nextjs-project/src/app/about/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About",
+};
+
 export default function AboutPage() {
     return (
         <div className="flex items-center justify-center min-h-screen">
