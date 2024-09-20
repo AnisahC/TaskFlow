00251689b4a08c4eefd2dc0e2c85bfebf36ab33b
SpindlyGold019 <58046529+SpindlyGold019@users.noreply.github.com>
2024-09-19 22:22:58 -0700

    Made a more proper 'about' page.

--- application/nextjs-project/src/app/about/page.tsx
+++ application/nextjs-project/src/app/about/page.tsx
@@ -1,5 +1,10 @@
 export default function AboutPage() {
     return (
-        <div>About Page Here</div>
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="text-center space-y-5">
+                <h1 className="text-4xl">We are CSC 648/848 Team 4</h1>
+                <p>Click on our names above to see our profiles</p>
+            </div>
+        </div>
     );
 }
