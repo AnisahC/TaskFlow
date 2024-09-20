184b21ea20576ed8b566050e606e3ac75fd34663
SpindlyGold019 <58046529+SpindlyGold019@users.noreply.github.com>
2024-09-19 21:35:48 -0700

    Converted h1 and h2 css values into tailwinds equivalent to fix values appying elsewhere.

--- application/nextjs-project/src/app/about/josh/page.tsx
+++ application/nextjs-project/src/app/about/josh/page.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
 const About = () => {
   return (
     <div className="container">
-      <h1>Joshua Abrenica</h1>
-      <h2>Scrum Master & Github Master</h2>
+      <h1 className="text-[2.5rem] font-bold text-[#3e007c]">Joshua Abrenica</h1>
+      <h2 className="text-[1.5rem] text-[#97770d]">Scrum Master & Github Master</h2>
     </div>
   );
 };
